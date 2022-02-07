6d64e59dbeec1c2e25d8199d42e946b5f729f028
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-07 23:08:28 +0530

    Added:Player checks for option

--- src/com/javaproject/SnakeLadder.java
+++ src/com/javaproject/SnakeLadder.java
@@ -1,14 +1,43 @@
 package com.javaproject;
 
+import java.util.Random;
+
 public class SnakeLadder {
 	
+	static final int noPlay = 0;
+	static final int ladder = 1;
+	static final int snake = 2;
+	
+	
 	public static void main(String[] args) {
-		int player1Position = 0;
+		int position = 0;
 		
-        int player1Dice = (int)(Math.floor(Math.random() * 10) % 6) + 1;
-        System.out.println("Dice rolled value: " + player1Dice);
+		Random ran = new Random();
+        int dice = ran.nextInt(6)+1;
+		System.out.println("dice: "+dice);
+        int optCheck = ran.nextInt(3);
+		System.out.println("optCheck: "+optCheck);
 		
+		if (optCheck == ladder) 
+		{
+			System.out.println("Ladder");
+			position = position + dice;
+		}
+		
+		else if (optCheck == snake) 
+		{
+			System.out.println("Snake");
+			position = position - dice;
+		}
+		else {
+			System.out.println("No Play");
+		}
+		if (position<0)
+			position=0;
+		System.out.println("position: "+position);
 	}
-	
-	
+
+        
+        
 }
+	
