a182dae767b9ea291e9628a0ea2e8b44f279ebdb
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-07 23:32:14 +0530

    Added:No of times dice roll to win the game

--- src/com/javaproject/SnakeLadder.java
+++ src/com/javaproject/SnakeLadder.java
@@ -3,11 +3,10 @@ package com.javaproject;
 import java.util.Random;
 
 public class SnakeLadder {
-	
 	static final int noPlay = 0;
 	static final int ladder = 1;
 	static final int snake = 2;
-	
+	static int count = 0;
 	
 	public static void main(String[] args) {
 		int position = 0;
@@ -15,6 +14,7 @@ public class SnakeLadder {
 		while (position < 100) {
 		Random ran = new Random();
         int dice = ran.nextInt(6)+1;
+        ++count; 
         
 		System.out.println("dice: " + dice);
         int optCheck = ran.nextInt(3);
@@ -40,11 +40,15 @@ public class SnakeLadder {
 		if (position < 0)
 			position = 0;
 		
-		System.out.println("position: " + position);
+		System.out.println("position: " + position+ "\nDice count" + count);
 		}       
         
 	}
 }
+	
+	
+	
+        
 	
 	
 
